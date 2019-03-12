c9eecf98a8062d7a9c19dd0b0c74751b35f76830
Anurag Shukla <shukla.anurag0006@gmail.com>
2019-03-13 03:01:53 +0530

    Edit item functionality added

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,4 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name = "about"),
+    path('delete/<int:item_id>', views.delete, name="delete"),
+    path('cross/<int:item_id>', views.cross, name="cross"),
+    path('uncross/<int:item_id>', views.uncross, name="uncross"),
+    path("edit/<int:item_id>", views.edit, name="edit")
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForms
 from django.contrib import messages
@@ -10,11 +10,42 @@ def home(request):
         form = ListForms(request.POST or None)
         if form.is_valid():
             form.save()
-            messages.success(request, "The item has been added to the list!")
+            messages.success(request, "Item is added to list!")
             all_items = List.objects.all
-            render(request, 'homepage.html', {'allItems': all_items})
 
     all_items = List.objects.all
     return render(request, 'homepage.html', {'allItems' : all_items})
 def about(request):
     return render(request, 'about.html', {})
+
+def delete(request, item_id):
+    item = List.objects.get(pk = item_id)
+    item.delete()
+    return redirect('home')
+
+def cross(request, item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = True
+    item.save()
+    return (redirect('home'))
+
+def uncross(request, item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = False
+    item.save()
+    return (redirect('home'))
+
+def edit(request, item_id):
+
+    item = List.objects.all().get(pk = item_id)
+
+    if request.method == 'POST':
+        form = ListForms(request.POST or None, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Item is Edited!")
+            all_items = List.objects.all
+            return redirect('home')
+
+    all_items = List.objects.all
+    return render(request, 'edit.html', {'item' : item})
