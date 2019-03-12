1899d1f3144cb3e7f1b1ed9846461c8ad74ec59a
Anurag Shukla <shukla.anurag0006@gmail.com>
2019-03-13 00:39:14 +0530

    Added Form

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class List(models.Model):
     item = models.CharField(max_length=200)
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForms
 
 # Create your views here.
 def home(request):
-    return render(request, 'homepage.html', {})
+
+    if request.method == 'POST':
+        form = ListForms(request.POST or None)
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            render(request, 'homepage.html', {'allItems': all_items})
+
+    all_items = List.objects.all
+    return render(request, 'homepage.html', {'allItems' : all_items})
 def about(request):
     return render(request, 'about.html', {})
